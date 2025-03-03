4e6b8125b01a467ad22498e9ad283685ceb2dd8f
Sagarika-02 <sagarikas901@gmail.com>
2025-03-04 02:32:35 +0530

    Connected to MySQL

--- myproject/settings.py
+++ myproject/settings.py
@@ -84,8 +84,16 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':os.getenv('DB_NAME'),
+        'USER':os.getenv('DB_USER_NAME'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'PORT':os.getenv('DB_PORT'),
+        'HOST':os.getenv('DB_HOST')
+        
+        
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
